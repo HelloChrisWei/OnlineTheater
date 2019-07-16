f17ce8b83135b76a7960f9e1c135ee1653958549
Chris Wei <weikang0827@hotmail.com>
2019-07-16 20:54:07 +0800

    upload initial configuration.
    Signed-off-by: Chris Wei <weikang0827@hotmail.com>

--- guns-common-api/src/main/java/com/stylefeng/guns/rest/modular/film/FilmServiceApi.java
+++ guns-common-api/src/main/java/com/stylefeng/guns/rest/modular/film/FilmServiceApi.java
@@ -1,4 +1,11 @@
 package com.stylefeng.guns.rest.modular.film;
+
+import com.stylefeng.guns.rest.modular.film.VO.BannerVO;
+import com.stylefeng.guns.rest.modular.film.VO.FilmInfo;
+import com.stylefeng.guns.rest.modular.film.VO.FilmVO;
+
+import java.util.List;
+
 /**
  * 
  * @version 1.0 2019/7/16
@@ -6,4 +13,16 @@ package com.stylefeng.guns.rest.modular.film;
  */
 public interface FilmServiceApi {
 
+    List<BannerVO> getBanners();
+
+    // 获取热映影片
+    FilmVO getHotFilms(boolean isLimit,int nums,int nowPage,int sortId,int sourceId,int yearId,int catId);
+    // 获取即将上映影片[受欢迎程度做排序]
+    FilmVO getSoonFilms(boolean isLimit,int nums,int nowPage,int sortId,int sourceId,int yearId,int catId);
+
+    List<FilmInfo> getBoxRanking();
+
+    List<FilmInfo> getExpectRanking();
+
+    List<FilmInfo> getTop();
 }
--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
@@ -1,9 +1,9 @@
 package com.stylefeng.guns.rest.modular.film;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.stylefeng.guns.rest.modular.film.VO.ConditionListResult;
 import com.stylefeng.guns.rest.modular.film.VO.FilmIndexVO;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
-import jdk.nashorn.internal.ir.annotations.Reference;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,31 +16,31 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/film")
 public class FilmController {
 
+    @Reference(interfaceClass = FilmServiceApi.class, check = false)
+    private FilmServiceApi filmServiceApi;
+
+
 
     @RequestMapping(value = "/getIndex", method = RequestMethod.GET)
     public ResponseVO<FilmIndexVO> getIndex() {
 
         FilmIndexVO filmIndexVO = new FilmIndexVO();
 
-//        // 获取banner信息
-//        filmIndexVO.setBanners(filmServiceApi.getBanners());
-//        // 获取正在热映的电影
-//        filmIndexVO.setHotFilms(filmServiceApi.getHotFilms(true, 4, 1, 1, 99, 99, 99));
-//        // 即将上映的电影
-//        filmIndexVO.setSoonFilms(filmServiceApi.getSoonFilms(true, 4, 1, 1, 99, 99, 99));
-//        // 票房排行榜
-//        filmIndexVO.setBoxRanking(filmServiceApi.getBoxRanking());
-//        // 获取受欢迎的榜单
-//        filmIndexVO.setExpectRanking(filmServiceApi.getExpectRanking());
-//        // 获取前一百
-//        filmIndexVO.setTop100(filmServiceApi.getTop());
+        // 获取banner信息
+        filmIndexVO.setBanners(filmServiceApi.getBanners());
+        // 获取正在热映的电影
+        filmIndexVO.setHotFilms(filmServiceApi.getHotFilms(true, 4, 1, 1, 99, 99, 99));
+        // 即将上映的电影
+        filmIndexVO.setSoonFilms(filmServiceApi.getSoonFilms(true, 4, 1, 1, 99, 99, 99));
+        // 票房排行榜
+        filmIndexVO.setBoxRanking(filmServiceApi.getBoxRanking());
+        // 获取受欢迎的榜单
+        filmIndexVO.setExpectRanking(filmServiceApi.getExpectRanking());
+        // 获取前一百
+        filmIndexVO.setTop100(filmServiceApi.getTop());
 
         return ResponseVO.success(filmIndexVO);
     }
-
-
-
-
 
 
 
